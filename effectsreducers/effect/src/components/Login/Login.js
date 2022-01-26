c90fd22e0bc4043ab1eb429a406aaf5410d48e7d
Sadiksha <sadiksha.chitrakar101@gmail.com>
2022-01-26 09:39:42 +0545

    Use Context and Providers

--- effectsreducers/effect/src/components/Login/Login.js
+++ effectsreducers/effect/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer} from 'react';
+import React, { useState, useEffect, useReducer,useContext} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer=(state,action)=>{
   if(action.type==='USER_INPUT'){
@@ -46,6 +47,8 @@ const Login = (props) => {
   const [emailState,dispatchEmail]=useReducer(emailReducer,{value:'',isValid:false,});
   const [passwordState,dispatchPassword]=useReducer(passwordReducer,{value:'',isValid:null,})
 
+  const authCtx=useContext(AuthContext);
+
   const emailChangeHandler = (event) => {
     dispatchEmail({type:'USER_INPUT',val:event.target.value});
     // setEnteredEmail(event.target.value);
@@ -76,7 +79,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
--- effectsreducers/effect/src/index.js
+++ effectsreducers/effect/src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { AuthContextProvider } from './store/auth-context';
 
 
 ReactDOM.render(
-    <App />,
+  <AuthContextProvider>
+      <App />
+  </AuthContextProvider>
+    ,
   document.getElementById('root')
 );
